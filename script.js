39316c922a11ee0c9b1e8e370d75ae43d40668af
Matheus <matheus.amaral.1443007@sga.pucminas.br>
2023-07-03 18:22:56 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+function toogleMode() {
+  let html = document.documentElement
+
+  if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  }
+
+  else {
+    html.classList.add('light')
+  }
+}
